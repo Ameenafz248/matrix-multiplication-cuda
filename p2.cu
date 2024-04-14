8c73b5efed99a9ad1bae9f48ddf8ae2623da0407
Mohammed Ameen <ma6681221@gmail.com>
2024-04-14 10:23:34 +0530

    updated p2.cu

--- p2.cu
+++ p2.cu
@@ -10,7 +10,6 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define BLOCK_SIZE 16
 
 /*
 *********************************************************************
@@ -70,11 +69,10 @@ void cpu_matrix_mult(int *h_a, int *h_b, int *h_result, int m, int n, int k) {
 
 int main(int argc, char const *argv[])
 {
-    int m, n, k;
+    int m = 512, n = 1024, k = 256;
     /* Fixed seed for illustration */
     srand(3333);
-    printf("please type in m n and k\n");
-    scanf("%d %d %d", &m, &n, &k);
+
 
     // allocate memory in host RAM, h_cc is used to store CPU result
     int *h_a, *h_b, *h_c, *h_cc;
@@ -97,44 +95,59 @@ int main(int argc, char const *argv[])
         }
     }
 
-    float gpu_elapsed_time_ms, cpu_elapsed_time_ms;
+	float gpu_unshared_elapsed_time_ms[3], gpu_shared_elapsed_time_ms[3], cpu_elapsed_time_ms;
 
     // some events to count the execution time
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
+	
 
-    // start to count execution time of GPU version
-    cudaEventRecord(start, 0);
-    // Allocate memory space on the device 
-    int *d_a, *d_b, *d_c;
-    cudaMalloc((void **) &d_a, sizeof(int)*m*n);
-    cudaMalloc((void **) &d_b, sizeof(int)*n*k);
-    cudaMalloc((void **) &d_c, sizeof(int)*m*k);
+	int block_size = 128;
+	int idx = 0;
+	printf("Time elapsed for unshared memory:\n\n");
+	for (; block_size <= 128; block_size *= 2) {
+		// start to count execution time of GPU version
+		cudaEventRecord(start, 0);
+		// Allocate memory space on the device 
 
-    // copy matrix A and B from host to device memory
-    cudaMemcpy(d_a, h_a, sizeof(int)*m*n, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, sizeof(int)*n*k, cudaMemcpyHostToDevice);
+		int *d_a, *d_b, *d_c;
+		cudaMalloc((void **) &d_a, sizeof(int)*m*n);
+		cudaMalloc((void **) &d_b, sizeof(int)*n*k);
+		cudaMalloc((void **) &d_c, sizeof(int)*m*k);
 
-    unsigned int grid_rows = (m + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    unsigned int grid_cols = (k + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    dim3 dimGrid(grid_cols, grid_rows);
-    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-   
+		// copy matrix A and B from host to device memory
+		cudaMemcpy(d_a, h_a, sizeof(int)*m*n, cudaMemcpyHostToDevice);
+		cudaMemcpy(d_b, h_b, sizeof(int)*n*k, cudaMemcpyHostToDevice);
 
-    gpu_matrix_mult<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, m, n, k);    
+		unsigned int grid_rows = (m + block_size - 1) / block_size;
+		unsigned int grid_cols = (k + block_size - 1) / block_size;
+		dim3 dimGrid(grid_cols, grid_rows);
+		dim3 dimBlock(block_size, block_size);
+	   
+
+		gpu_matrix_mult<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, m, n, k);    
 
 
-    // Transefr results from device to host 
-    cudaMemcpy(h_c, d_c, sizeof(int)*m*k, cudaMemcpyDeviceToHost);
-    cudaThreadSynchronize();
-    // time counting terminate
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
+		// Transefr results from device to host 
+		cudaMemcpy(h_c, d_c, sizeof(int)*m*k, cudaMemcpyDeviceToHost);
+		cudaThreadSynchronize();
+		// time counting terminate
+		cudaEventRecord(stop, 0);
+		cudaEventSynchronize(stop);
 
-    // compute time elapse on GPU computing
-    cudaEventElapsedTime(&gpu_elapsed_time_ms, start, stop);
-    printf("Time elapsed on matrix multiplication of %dx%d . %dx%d on GPU: %f ms.\n\n", m, n, n, k, gpu_elapsed_time_ms);
+		// compute time elapse on GPU computing
+		cudaEventElapsedTime(&gpu_unshared_elapsed_time_ms[idx], start, stop);
+		printf("\tTime elapsed on GPU with %d threads/block: %f ms.\n\n", block_size, m, n, n, k, gpu_unshared_elapsed_time_ms[idx]);
+		
+		/* // free memory */
+		/* cudaFree(d_a); */
+		/* cudaFree(d_b); */
+		/* cudaFree(d_c); */
+		idx++;
+
+	}
+
 
     // start the CPU version
     cudaEventRecord(start, 0);
@@ -164,17 +177,19 @@ int main(int argc, char const *argv[])
     // roughly compute speedup
     if(all_ok)
     {
-        printf("all results are correct!!!, speedup = %f\n", cpu_elapsed_time_ms / gpu_elapsed_time_ms);
+        printf("all results are correct!\n");
+		printf("unshared speedup:\n");
+		for (auto i = 0; i < 3; ++i) {
+				
+			printf("%f: %f\n", std::pow(2, 7 + i), cpu_elapsed_time_ms / gpu_unshared_elapsed_time_ms[i]);
+		}
     }
     else
     {
         printf("incorrect results\n");
     }
 
-    // free memory
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_c);
+
     cudaFreeHost(h_a);
     cudaFreeHost(h_b);
     cudaFreeHost(h_c);
